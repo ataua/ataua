399cef691daca4bf67fb96fa6ecc6b362bf3923a
Atauã Pinali Doederlein <ataua.doederlein@gmail.com>
2025-08-26 19:26:17 -0300

    add linkedin curriculum, put date in generatepdf.sh

--- generatepdf.sh
+++ generatepdf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # output=$1 
-output="curriculum.pdf" 
+output="curriculum-$(date -Idate).pdf" 
 pandoc ./README.md --pdf-engine=xelatex --include-in-header=header.tex -o $output > log.txt 2>&1
